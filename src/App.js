f4ba62922b669e7a8caf23065301dbc3999d7adf
st44100 <st44100@gmail.com>
2017-12-11 19:09:51 +0900

    credencial include

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     fetch(LOGIN_ENDPOINT,{
       method: 'POST',
       mode: 'cors',
-      // credentials: 'include',
+      credentials: 'include',
       cache: 'default',
       body: body
     })
